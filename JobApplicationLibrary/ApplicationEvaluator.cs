d7312a774778b528e2f46fc28a3e964191f7afdb
fundasarikaya <fundasarikaya58@gmail.com>
2023-03-16 00:32:07 +0300

    Senaryo oluşturuldu, metotların unit testleri yazıldı,mock ve setup kavramları incelendi

---
+++ JobApplicationLibrary/ApplicationEvaluator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JobApplicationLibrary.Models;
+using JobApplicationLibrary.Services;
+
+namespace JobApplicationLibrary
+{
+    public class ApplicationEvaluator
+    {
+        private const int minAge = 18;
+        private const int autoAcceptedYearOfExperience = 15;
+        private readonly IIdentityValidator identityValidator;
+        private List<string> techStackList = new List<string>() { "C#", "RabbmitMQ", "Microservice", "Visual Studio" };
+        public ApplicationEvaluator(IIdentityValidator identityValidator)
+        {
+            this.identityValidator = identityValidator;
+            //test metotunda burası aslında bir bağımlılığı ifade eder,classın newlenmesi,test metotdu başka bir servise bağımlı olmuş oluyor
+            //dışardan parametre olarak gonderilmesi saglandıgında ben kontrol edebilirim dedigimizden test kısmı daha saglıklı olur
+            //gerçek servise gidip gec cevap gelmesi ,yada çalışmıyor olması gibi ihtimallerden kaynaklı moq yapısı kullanılır
+        }
+
+        public ApplicationResult Evaluate(JobApplication form)
+        {
+
+            if (form.Applicant.Age < minAge)
+                return ApplicationResult.AutoRejected;
+
+            var validIdentity = identityValidator.IsValid(form.Applicant.identityNumber);
+
+            if (!validIdentity)
+                return ApplicationResult.TransferredToHR;
+
+            var sr = GetTechStackSimilarityRate(form.TechStackList);
+
+            if (sr < 25)
+                return ApplicationResult.AutoRejected;
+
+            if (sr > 75 && form.YearsOfExperience >= autoAcceptedYearOfExperience)
+                return ApplicationResult.AutoAccepted;
+
+            return ApplicationResult.AutoAccepted;
+        }
+
+        private int GetTechStackSimilarityRate(List<string> techStacks)
+        {
+            var matchedCount =
+                 techStacks
+                    .Where(i => techStackList.Contains(i, StringComparer.OrdinalIgnoreCase))
+                    .Count();
+           
+            return (int)((double)matchedCount / techStackList.Count * 100);
+        }
+    }
+}
---
+++ UnitTestMocking/ApplicationEvaluateUnitTest.cs
@@ -0,0 +1,111 @@
+﻿using JobApplicationLibrary;
+using JobApplicationLibrary.Models;
+using JobApplicationLibrary.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestMocking.UnitTest
+{
+    [TestClass]
+    public class ApplicationEvaluateUnitTest
+    {
+        [TestMethod]
+        //Method Name Pattern : UnitOfWork_Condition_ExpectedResult 
+        public void Application_WithUnderAge_TransferredToAutoRejected()
+        {
+            //Arrange
+            var evaluator = new ApplicationEvaluator(null);
+            var form = new JobApplication()
+            {
+                Applicant = new JobApplicationLibrary.Models.Applicant()
+                {
+                    Age = 17
+                }
+            };
+
+            //Action
+            var appResult = evaluator.Evaluate(form);
+
+            //Assert
+            Assert.AreEqual(appResult, ApplicationResult.AutoRejected);
+
+        }
+        [TestMethod]
+        public void Application_WithNoTechStack_TransferredToAutoRejected()
+        {
+            //Arrange
+
+            var mockValidator = new Mock<IIdentityValidator>();
+            
+            //boyle bir interface varmışta onun içindeki metotlara ulaşabiliyormuşuz gibi davranır.
+            //mockValidator.Object dediğimizde IIdentityValidator interfaceini vermiş olur
+
+            mockValidator.Setup(i => i.IsValid(It.IsAny<string>())).Returns(true);
+            
+            //bu methodu burda kullanacagım dedik setup ile
+            //sahte bir obje oluşturup onun metodunu kullandık ve true donsun dedik
+            //herhangi bir şey gönderildiğinde true dönsün dedik
+
+            var evaluator = new ApplicationEvaluator(mockValidator.Object);
+            var form = new JobApplication()
+            {
+                Applicant = new JobApplicationLibrary.Models.Applicant() { Age = 19, identityNumber = "123" },
+                TechStackList = new System.Collections.Generic.List<string>() { "" }
+            };
+
+            //Action
+            var appResult = evaluator.Evaluate(form);
+
+            //Assert
+            Assert.AreEqual(appResult, ApplicationResult.AutoRejected);
+        }
+        [TestMethod]
+        public void Application_WithTechStackOver75P_TransferredToAutoAccepted()
+        {
+            //Arrange
+            var mockValidator = new Mock<IIdentityValidator>();
+            mockValidator.Setup(i => i.IsValid(It.IsAny<string>())).Returns(true);
+
+            var evaluator = new ApplicationEvaluator(mockValidator.Object);
+            var form = new JobApplication()
+            {
+                Applicant = new Applicant() { Age = 19 },
+                TechStackList = new System.Collections.Generic.List<string>()
+                {
+                    "C#", "RabbitMQ", "Microservice", "Visual Studio"
+                },
+                YearsOfExperience = 16
+            };
+
+            //Action
+            var appResult = evaluator.Evaluate(form);
+
+            //Assert
+            Assert.AreEqual(appResult, ApplicationResult.AutoAccepted);
+        }
+        [TestMethod]
+        public void Application_WithInValidIdentityNumber_TransferredToHR()
+        {
+            //Arrange
+            var mockValidator = new Mock<IIdentityValidator>();
+            mockValidator.Setup(i => i.IsValid(It.IsAny<string>())).Returns(false);
+
+            var evaluator = new ApplicationEvaluator(mockValidator.Object);
+            var form = new JobApplication()
+            {
+                Applicant = new JobApplicationLibrary.Models.Applicant() { Age = 19 }
+            };
+
+            //Action
+            var appResult = evaluator.Evaluate(form);
+            //Assert
+            Assert.AreEqual(appResult, ApplicationResult.TransferredToHR);
+        }
+    }
+}
